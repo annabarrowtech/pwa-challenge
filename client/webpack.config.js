af0d7ee3cc48da44162cf12232ef652da64af78e
abarrow1016 <annabarrowmusic@gmail.com>
2022-10-25 17:54:38 -0500

    add css loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,25 +48,25 @@ module.exports = () => {
       ],
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+          module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    // We use babel-loader in order to use ES6.
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
